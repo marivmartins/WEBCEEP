8516cb0441c83c8eead62ff349617a60ee3eeea2
Guizer0h <hara.guilherme@escola.pr.gov.br>
2026-03-01 16:41:58 -0300

    Refactor scroll event handling in fetec.js

--- fetec.js
+++ fetec.js
@@ -21,23 +21,3 @@ window.addEventListener('scroll', () => {
         logo.classList.add('visivel');
     }
 });
-
-const slides = document.querySelectorAll('.slide');
-const setaEsquerda = document.querySelector('.seta.esquerda');
-const setaDireita = document.querySelector('.seta.direita');
-let index = 0;
-
-function mostrarSlide(i) {
-    slides.forEach(slide => slide.classList.remove('ativo'));
-    slides[i].classList.add('ativo');
-}
-
-setaDireita.addEventListener('click', () => {
-    index = (index + 1) % slides.length;
-    mostrarSlide(index);
-});
-
-setaEsquerda.addEventListener('click', () => {
-    index = (index - 1 + slides.length) % slides.length;
-    mostrarSlide(index);
-});
